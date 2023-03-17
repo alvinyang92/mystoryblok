c71e8d32e8d1ada9330c654430cda09a73cf2f0d
alvinyang92 <alvinngtzeyang@gmail.com>
2023-03-17 10:17:02 +0800

    update

---
+++ nuxt.config.js
@@ -0,0 +1,15 @@
+export default defineNuxtConfig({
+  css: ['@/assets/css/roboto.css'],
+  modules: [
+    [
+      '@storyblok/nuxt',
+      {
+        accessToken: 'NrFmfS0ZdNiSSMbrJx6Qjwtt',
+        // apiOptions: {
+        //   region: "us" // When creating your space in US region (EU default)
+        // }
+      },
+    ],
+    '@nuxtjs/tailwindcss',
+  ],
+})
